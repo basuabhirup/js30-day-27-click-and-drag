d2056efdeb44b57ef4e7da350f152059e048d372
basuabhirup <basu.abhirup@yahoo.com>
2021-09-02 13:17:28 +0530

    Complete Project.

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mouseleave', () => {
@@ -18,8 +20,10 @@ slider.addEventListener('mouseup', () => {
   slider.classList.remove('active');
 });
 
-slider.addEventListener('mousemove', () => {
+slider.addEventListener('mousemove', (e) => {
   if(isDown) {
-
+    e.preventDefault(); // to prevent selecting any text
+    const x = e.pageX - slider.offsetLeft;
+    slider.scrollLeft = scrollLeft + (startX - x) * 3;
   }
 });
